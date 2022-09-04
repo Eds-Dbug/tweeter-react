c6e752cefe10b804ff34005c07c42367b639777f
Eds-Dbug <edsdbug@gmail.com>
2022-09-04 20:22:51 +0000

    react tweeter commt

---
+++ react-version/src/App.js
@@ -0,0 +1,17 @@
+import Navigation from "./components/Navigation";
+import Profile from "./components/Profile";
+import TweetForm from "./components/TweetForm";
+import TweetList from "./components/TweetList"
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Profile />
+      <TweetForm />
+      <TweetList />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,7 @@
+function TweetForm() {
+  return(
+    <h1>I am the TweetForm</h1>
+  );
+}
+
+export default TweetForm;
